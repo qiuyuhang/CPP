6d8b4ce6d181ef9082689c7601c0112deabda9b0
qiuyuhang <qyh@tongji.edu.cn>
2016-04-08 22:02:28 +0800

    Update 1.2.cpp
    bug fix

--- OJ/1.2.cpp
+++ OJ/1.2.cpp
@@ -7,7 +7,7 @@ int main()
 	cin >> n >> k;
 	cout << ((n - 1)*(n - 1) + (n + 1)*k)*n / 2 << endl;
 	int x = 1, y = n*(k - 1) + 1;
-	while (y < n*n) {
+	while (y <= n*n) {
 		for (int a = 0; a<k - 1; a++) {
 			cout << x++ << " ";
 		}
